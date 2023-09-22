23dc76ed5ffa148f2cf3d0ec0c1f3605f530486e
jeongwan89 <jeongwan89@mac.com>
2023-09-23 00:07:44 +0900

    거의 마지막 단계

--- timelapseCam1.sh
+++ timelapseCam1.sh
@@ -9,19 +9,22 @@ SNAPSHOT_PATH="$SNAPSHOTS_DIR/$DAY/$DATE.jpg"
 CURRENT_PATH="$SNAPSHOTS_DIR/current.jpg"
 
 if [ ! -d "$SNAPSHOTS_DIR" ] ; then
-    echo 디렉토리가 존재하지 않는다.
+    # echo 디렉토리가 존재하지 않는다.
     echo eerrtt | sudo -S mkdir "$SNAPSHOTS_DIR"
-else
-    echo 디렉토리가 존재한다.
+# else
+    # echo 디렉토리가 존재한다.
 fi
 
 if [ ! -e "$SNAPSHOTS_DIR/numbering.buff" ] ; then # 파일이 존재하지 않으면
-    # echo "파일이 없다고 해서 들어왔습니다."
+    # echo "numbering.buff 파일이 없다고 해서 들어왔습니다."
     echo eerrtt | sudo -S touch "$SNAPSHOTS_DIR/numbering.buff"
     echo eerrtt | sudo -S echo $DAY | sudo tee "$SNAPSHOTS_DIR/numbering.buff" > /dev/null  # 파일에 덮어 쓰기를 먼저하고
     echo eerrtt | sudo -S echo 1 | sudo tee -a "$SNAPSHOTS_DIR/numbering.buff" > /dev/null  # 나중에 추가하여 기록한다.
     FREADDAY=$DAY
-    FREADNUM=1
+    FREADNUM=0
+    # 그리고 $DAY 디렉토리를 만든다.
+    # ? numbering.buff 파일이 없다고 해서 오늘 날짜 디렉토리가 없다면 어떻게 하지? -> 그러면 디렉토리를 만들어야지!
+    echo eerrtt | sudo -s mkdir -p "$SNAPSHOTS_DIR"/"$DAY"
 else
     # 파일이 존재하면,while로 읽는다. 한꺼번에 읽으니까 배열을 이용하자 ARRAY[]
     # 배열활용때는 ${Arr[0...3]}의 형태로 변수를 참조할 수 있다.
