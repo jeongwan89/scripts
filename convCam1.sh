273b188b7a4f2b5e5a84c03ec3b868bfd162fc84
jeongwan89 <jeongwan89@mac.com>
2023-09-24 16:18:20 +0900

    yesterday

--- convCam1.sh
+++ convCam1.sh
@@ -1,5 +1,4 @@
-DAY=$(date +"%Y-%m-%d")
-DAY=$((DAY-1))
+YESTERDAY=$(date +"%Y-%m-%d" -d '1-day')
 SNAPSHOTS_DIR=/mnt/timelapse/camera1
 SNAPSHOT_PATH="$SNAPSHOTS_DIR/$DAY"
 echo eerrtt | sudo -S ffmpeg -framerate 24 -pattern_type glob -i "$SNAPSHOT_PATH/*.jpg" -s:v 1280X960 -c:v libx264 -crf 17 -pix_fmt yuv420p "$SNAPSHOTS_DIR/$DAY.mp4"
