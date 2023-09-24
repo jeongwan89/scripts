7ce245da494b0946ca3adc7887d17a39bd26cacc
jeongwan89 <jeongwan89@mac.com>
2023-09-24 09:44:59 +0900

    조금더 구조화

--- timelapseCam1.sh
+++ timelapseCam1.sh
@@ -16,18 +16,22 @@ if [ ! -d "$SNAPSHOTS_DIR" ] ; then
     # echo 디렉토리가 존재한다.
 fi
 
+# 참고로 if절 내에서 변수를 생성하려면,
+# if-yes의 변수 생성 구조와
+# if-else의 변수 생성 구조가 일치해야 된다. 이유는 구절 밖에서 변수 생성이 되지 않으므로, 스크립의 consistence를 유지하려면 그렇다.
 if [ ! -e "$SNAPSHOTS_DIR/numbering.buff" ] ; then # 파일이 존재하지 않으면
     # echo "numbering.buff 파일이 없다고 해서 들어왔습니다."
     echo eerrtt | sudo -S touch "$SNAPSHOTS_DIR/numbering.buff"
     echo eerrtt | sudo -S echo $DAY | sudo tee "$SNAPSHOTS_DIR/numbering.buff" > /dev/null  # 파일에 덮어 쓰기를 먼저하고
     echo eerrtt | sudo -S echo 1 | sudo tee -a "$SNAPSHOTS_DIR/numbering.buff" >> /dev/null  # 나중에 추가하여 기록한다.
-    FREADDAY=$DAY
-    FREADNUM=0
+    FREADDAY=$DAY   # File READ DAY
+    FREADNUM=0      # File READ NUMber
     # 그리고 $DAY 디렉토리를 만든다.
     # ? numbering.buff 파일이 없다고 해서 오늘 날짜 디렉토리가 없다면 어떻게 하지? -> 그러면 디렉토리를 만들어야지!
     echo eerrtt | sudo -s mkdir -p "$SNAPSHOTS_DIR"/"$DAY"
 else
-    # 파일이 존재하면,while로 읽는다. 한꺼번에 읽으니까 배열을 이용하자 ARRAY[]
+    # 파일이 존재하면. 
+    # 한꺼번에 읽으니까 배열을 이용하자 ARRAY[]
     # 배열활용때는 ${Arr[0...3]}의 형태로 변수를 참조할 수 있다.
     readarray -t Arr < "$SNAPSHOTS_DIR/numbering.buff"
     FREADDAY=${Arr[0]}
@@ -38,7 +42,7 @@ if [ $DAY == $FREADDAY ] ; then
     # 같은 날에 기록한다.
     FREADNUM=$(($FREADNUM + 1))
     echo eerrtt | sudo -S echo $DAY | sudo tee "$SNAPSHOTS_DIR/numbering.buff" > /dev/null  # 파일에 덮어 쓰기를 먼저하고
-    echo eerrtt | sudo -S echo $FREADNUM | sudo tee -a "$SNAPSHOTS_DIR/numbering.buff" >> /dev/null  # 나중에 추가하여 기록한다.
+    echo eerrtt | sudo -S echo $FREADNUM | sudo tee -a "$SNAPSHOTS_DIR/numbering.buff" > /dev/null  # 나중에 추가하여 기록한다.
 else
     # 다른 날이라면. (일반적인 경우 하루 넘긴 날이라면)
     # 1. 새로운 디렉토리를 만들고
@@ -46,7 +50,7 @@ else
     # 2. numbering.buff에 날짜를 새로 쓰고
     echo eerrtt | sudo -S echo $DAY | sudo tee "$SNAPSHOTS_DIR/numbering.buff" > /dev/null
     # 3. 기억할 숫자로는 1을 대입한다.
-    echo eerrtt | sudo -S echo 1 | sudo tee -a "$SNAPSHOTS_DIR/numbering.buff" >> /dev/null  # 나중에 추가하여 기록한다.
+    echo eerrtt | sudo -S echo 1 | sudo tee -a "$SNAPSHOTS_DIR/numbering.buff" > /dev/null  # 나중에 추가하여 기록한다.
     FREADNUM=1
 fi
 
