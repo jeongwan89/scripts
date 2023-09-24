adfced42a529f877f86f5a47592005fdc6f1910d
jeongwan89 <jeongwan89@mac.com>
2023-09-24 16:22:07 +0900

    어제 파일을 정리하여 타임랩스

--- convCam1.sh
+++ convCam1.sh
@@ -1,4 +1,5 @@
-YESTERDAY=$(date +"%Y-%m-%d" -d '1-day')
+YESTERDAY=$(date +"%Y-%m-%d" -d '1 day ago')
+echo $YESTERDAY
 SNAPSHOTS_DIR=/mnt/timelapse/camera1
-SNAPSHOT_PATH="$SNAPSHOTS_DIR/$DAY"
-echo eerrtt | sudo -S ffmpeg -framerate 24 -pattern_type glob -i "$SNAPSHOT_PATH/*.jpg" -s:v 1280X960 -c:v libx264 -crf 17 -pix_fmt yuv420p "$SNAPSHOTS_DIR/$DAY.mp4"
+SNAPSHOT_PATH="$SNAPSHOTS_DIR/$YESTERDAY"
+echo eerrtt | sudo -S ffmpeg -y -framerate 24 -pattern_type glob -i "$SNAPSHOT_PATH/*.jpg" -s:v 1280X960 -c:v libx264 -crf 17 -pix_fmt yuv420p "$SNAPSHOTS_DIR/$YESTERDAY.mp4"
