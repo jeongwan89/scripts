cb817f687ff75cafc341ab401c175c2362ac835a
jeongwan89 <jeongwan89@mac.com>
2023-09-24 15:51:47 +0900

    새로운 스크립 작성

---
+++ convCam1.sh
@@ -0,0 +1,4 @@
+DAY=$(date +"%Y-%m-%d")
+SNAPSHOTS_DIR=/mnt/timelapse/camera1
+SNAPSHOT_PATH="$SNAPSHOTS_DIR/$DAY"
+echo eerrtt | sudo -S ffmpeg -framerate 24 -pattern_type glob -i "$DAY/*.jpg" -s:v 1280X960 -c:v libx264 -crf 17 -pix_fmt yuv420p "$SNAPSHOTS_DIR/$DAY.mp4"
